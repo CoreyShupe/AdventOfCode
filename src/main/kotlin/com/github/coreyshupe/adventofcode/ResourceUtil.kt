bfa925143a920cc0d82e74c019613061b7ff076a
Corey Shupe <coreyshupe101@gmail.com>
2018-12-02 13:48:16 -0500

    Maintenance (prettier ResourceUtil)

--- src/main/kotlin/com/github/coreyshupe/adventofcode/ResourceUtil.kt
+++ src/main/kotlin/com/github/coreyshupe/adventofcode/ResourceUtil.kt
@@ -3,7 +3,6 @@ package com.github.coreyshupe.adventofcode
 fun String.asResource(applier: (String) -> Unit) =
     applier.invoke(this::class.java.getResource(this).readText().replace("\r", ""))
 
-fun String.asLinedResource(applier: (List<String>) -> Unit) =
-    this.asResource {
-        applier.invoke(it.split('\n'))
-    }
+fun String.asLinedResource(applier: (List<String>) -> Unit) = this.asResource {
+    applier.invoke(it.split('\n'))
+}
