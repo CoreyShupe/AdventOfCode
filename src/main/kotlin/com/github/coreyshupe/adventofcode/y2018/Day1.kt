2d9a5cc7fac08947efdd9a9124d6e447ab56c178
Corey Shupe <coreyshupe101@gmail.com>
2018-12-05 01:37:06 -0500

    Removed useless comments, replacing with javadoc later.

--- src/main/kotlin/com/github/coreyshupe/adventofcode/y2018/Day1.kt
+++ src/main/kotlin/com/github/coreyshupe/adventofcode/y2018/Day1.kt
@@ -16,24 +16,19 @@ fun applyFrequencies(input: List<String>) = input.map { it.toInt() }.sum()
 fun findRepeatingFrequency(input: List<String>): Int {
     val parsedIter = input.map { it.toInt() }.iterator()
 
-    // we want all of the frequencies as labeled
     val sums = generateSequence(0) {
         it + parsedIter.next()
     }.take(input.size + 1).toList()
 
-    // if the recurring item is in the first iter we want to return it here
     val first = sums.groupingBy { it }.eachCount().toList().firstOrNull { it.second > 1 }
     if (first != null) return first.first
 
     val shift = sums[sums.size - 1]
 
-    // if the shift is 0, then the recurring value is 0 along with -> else
     if (shift == 0) return 0
 
-    // populate a map of the groups (value % shift) will be the key for groups
     val map = sums.dropLast(1).mapIndexed { i, j -> i to j }.group { it.second % shift }
 
-    // These will be used later to find the best result
     fun Triple<Int, Int, Int>.compareTo(that: Triple<Int, Int, Int>) =
         if (this.first == that.first) this.second - that.second else this.first - that.first
 
@@ -42,8 +37,6 @@ fun findRepeatingFrequency(input: List<String>): Int {
 
     var minInfo: Triple<Int, Int, Int>? = null
     map.values.filter { it.size > 1 }.forEach { list ->
-        // Pairs in these have first(index) second(frequency)
-        // So we sort by the frequency
         val sortedList = list.sortedBy { it.second }
         val min = sortedList.drop(1).mapIndexed { i, pair ->
             val previous = sortedList[i]
@@ -55,5 +48,5 @@ fun findRepeatingFrequency(input: List<String>): Int {
         }.minWith(Comparator(Triple<Int, Int, Int>::compareTo))!!
         if (min.betterThan(minInfo)) minInfo = min
     }
-    return minInfo?.third ?: shift+1 // special case where no groups have more than 1 member defaults to shift + 1
+    return minInfo?.third ?: shift+1
 }
