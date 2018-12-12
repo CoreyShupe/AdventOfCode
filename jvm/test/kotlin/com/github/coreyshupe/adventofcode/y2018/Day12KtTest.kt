44954894263dae107277f7ec561a88c29f3ecd07
Corey Shupe <coreyshupe101@gmail.com>
2018-12-12 00:58:54 -0500

    Better day 12 tests, travis hates getResource

--- jvm/test/kotlin/com/github/coreyshupe/adventofcode/y2018/Day12KtTest.kt
+++ jvm/test/kotlin/com/github/coreyshupe/adventofcode/y2018/Day12KtTest.kt
@@ -1,10 +1,7 @@
 package com.github.coreyshupe.adventofcode.y2018
 
-import com.github.coreyshupe.adventofcode.ResourceType
-import com.github.coreyshupe.adventofcode.input
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 class Day12KtTest {
 
@@ -12,10 +9,43 @@ class Day12KtTest {
     fun findActivePotsTest() {
         // had to use these inputs, the examples given didn't really cover anything unit-testable
         // although these answers were confirmed by AoC
-        input(18, 12, ResourceType.Lined) {
-            val initial = it[0].split(' ')[2]
-            assertEquals(3915, findActivePots(initial, it.drop(2), 20))
-            assertEquals(4900000001793, findActivePots(initial, it.drop(2), 50_000_000_000))
+        """initial state: #....#.#....#....#######..##....###.##....##.#.#.##...##.##.#...#..###....#.#...##.###.##.###...#..#
+
+#..#. => #
+#...# => #
+.##.# => #
+#.... => .
+..#.. => .
+#.##. => .
+##... => #
+##.#. => #
+.#.## => #
+.#.#. => .
+###.. => .
+#..## => .
+###.# => .
+...## => .
+#.#.. => #
+..... => .
+##### => #
+..### => .
+..#.# => #
+....# => .
+...#. => #
+####. => #
+.#... => #
+#.#.# => #
+.##.. => #
+..##. => .
+##..# => .
+.#..# => #
+##.## => #
+.#### => .
+.###. => #
+#.### => .""".trimIndent().split('\n').apply {
+            val initial = this[0].split(' ')[2]
+            assertEquals(3915, findActivePots(initial, drop(2), 20))
+            assertEquals(4900000001793, findActivePots(initial, drop(2), 50_000_000_000))
         }
     }
 }
