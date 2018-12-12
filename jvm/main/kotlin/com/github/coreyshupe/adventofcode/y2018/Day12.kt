3c03c3ecb0c50c3c44301ba2b2dc07fd2ea4d8c0
Corey Shupe <coreyshupe101@gmail.com>
2018-12-12 01:36:37 -0500

    a bit better day12

--- jvm/main/kotlin/com/github/coreyshupe/adventofcode/y2018/Day12.kt
+++ jvm/main/kotlin/com/github/coreyshupe/adventofcode/y2018/Day12.kt
@@ -28,7 +28,7 @@ fun findActivePots(initial: String, input: List<String>, iters: Long): Long {
     var count = 0
     (1..iters).forEach { z ->
         val change = mutableSetOf<Int>()
-        for (x in (map.minBy { it.key }!!.key - 3)..(map.maxBy { it.key }!!.key + 3)) if (rules.contains(
+        for (x in (map.minBy { it.key }!!.key - 2)..(map.maxBy { it.key }!!.key + 2)) if (rules.contains(
                 FlowerPotRule(
                     Pair(map.getOrDefault(x - 2, false), map.getOrDefault(x - 1, false)),
                     map.getOrDefault(x, false),
@@ -41,11 +41,11 @@ fun findActivePots(initial: String, input: List<String>, iters: Long): Long {
         val curr = map.toList().sumBy { if (it.second) it.first else 0 }
         val diff = curr - last
         count = if (diff == lastDiff) (count + 1) else 0
-        if (count == 25) return (((iters - z) * diff) + curr)
+        if (count == 50) return (((iters - z) * diff) + curr)
         lastDiff = diff
         last = curr
     }
-    return map.toList().sumBy { if (it.second) it.first else 0 }.toLong()
+    return last.toLong()
 }
 
 private fun Char.check() = this == '#'
